2302e1bbfb1ef030a7222dbc9e63856ef4184fe0
ririyondesoleil <sayri.chotel@etud.univ-pau.fr>
2025-04-22 17:26:43 +0200

    DetailVC.swift

--- DetailVC.swift
+++ DetailVC.swift
@@ -13,9 +13,13 @@ class DetailVC: UIViewController {
     @IBOutlet weak var lblEu: UILabel!
 
     // Données envoyées depuis la TableView : permet d’afficher le mot sélectionné dans la table
-    var original: String = "" // Valeur non-optionnelle
+    var original: String = ""
     // Permet d’afficher la traduction basque correspondante
     var translation: String = ""
+    // Supprime à la bonne position, on met "?" car l'index peut être absent
+    var indexDeleteTrans: Int?
+    // Référence vers VisualtionTVC pour stocker
+    var visualisationVC: VisualisationTVC?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,16 +27,42 @@ class DetailVC: UIViewController {
         lblFr.text = original
         lblEu.text = translation
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func playFrAudio(_ sender: Any) {
+        guard let text = lblFr.text, !text.isEmpty else {
+            return
+        }
+        CloudAPI.audio(text: text, targetLang: "fr-FR")
     }
-    */
+
+    @IBAction func playEuAudio(_ sender: Any) {
+        guard let text = lblEu.text, !text.isEmpty else {
+            return
+        }
+        CloudAPI.audio(text: text, targetLang: "eu-ES")
+    }
+
+    @IBAction func deleteTrans(_ sender: Any) {
+        guard let index = indexDeleteTrans else {
+            return
+        }
+
+        // Supprime la bonne paire dans le dictionnaire
+        let sorted = AppDelegate.asSortedArr()
+        let pair = sorted[index]
+        let fr = pair[0]
+
+        // Supprime du dictionnaire
+        AppDelegate.translations.removeValue(forKey: fr)
+
+        // Enregistre dans le fichier
+        AppDelegate.writeTrans()
+
+        // Recharge l'affichage'
+        visualisationVC?.loadTableView()
+
+        // Retour à la page précédente
+        navigationController?.popViewController(animated: true)
+    }
 
 }
