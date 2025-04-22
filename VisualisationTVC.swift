871dbcdf875066c23afc80726388c29616be94d5
ririyondesoleil <sayri.chotel@etud.univ-pau.fr>
2025-04-22 17:26:18 +0200

    VisualisationTVC.swift

--- VisualisationTVC.swift
+++ VisualisationTVC.swift
@@ -60,41 +60,6 @@ class VisualisationTVC: UITableViewController {
         return cell
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -106,11 +71,12 @@ class VisualisationTVC: UITableViewController {
                 let wordEu = listTranslations[indexPath.row][1]
                 destinationVC.original = wordFr
                 destinationVC.translation = wordEu
-                /*
-                let wordsTranslated = AppDelegate.asSortedArr()
-                destinationVC.word = wordsTranslated[indexPath.row][0]
-                destinationVC.translation = wordsTranslated[indexPath.row][1]
-                 */
+
+                // Transmet l'index de la ligne à supprimer
+                destinationVC.indexDeleteTrans = indexPath.row
+
+                // Transmet une référence vers DetailVC pour pouvoir recharger la table après la suppression
+                destinationVC.visualisationVC = self
             }
         }
     }
